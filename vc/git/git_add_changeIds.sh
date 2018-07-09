1cf9b4137efa135c8ed1127e869cc0b9e1f98233
flobee <flobee@debian>
2018-07-09 20:52:19 +0200

    Adds git helper

---
+++ vc/git/git_add_changeIds.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# adds Change-Id to commits which are not pushed yet
+#
+tmp=$(mktemp)
+hook=$(readlink -f $(git rev-parse --git-dir))/hooks/commit-msg
+git filter-branch -f --msg-filter "cat > $tmp; \"$hook\" $tmp; cat $tmp" @{u}..HEAD
+
